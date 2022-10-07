70f2c6dbd45f55b9d73cececf134db691ab5c053
Juan Gómez <gomezj01@stepstone.com>
2022-10-07 09:01:12 -0600

    Se agrega pantalla de login

---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from '@components/LoginPage';
+
+const login = () => {
+  return (
+    <>
+        <LoginPage />
+    </>
+  )
+}
+
+export default login;
